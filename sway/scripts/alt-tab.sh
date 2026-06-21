3302e6e1bd4145ef80ccad1d4a88754d8ce1d0c2
László Kovács <lszlokovacs@gmail.com>
2026-06-21 23:25:27 +0300

    Delete sway directory

--- sway/scripts/alt-tab.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-rofi -show combi \
-  -combi-modes window,drun,run \
-  -modes combi \
-  -show-icons \
-  -display-combi "Alt-Tab"
--- sway/scripts/launcher.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#!/usr/bin/env bash
-
-rofi -show run \
-  -show-icons \
-  -theme ~/.config/rofi/config.rasi
